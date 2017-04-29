c231b4e6c8d5b6a64a333c00562ae867f85bc012
Yamagishi Kazutoshi <ykzts@desire.sh>
2017-04-29 15:49:06 +0900

    improve update mastodon task

--- scripts/update-mastodon.sh
+++ scripts/update-mastodon.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+set -ex
+
 cd $(dirname $0)/..
 
-docker-compose pull
-docker-compose down
+docker-compose pull web streaming sidekiq
+docker-compose stop web streaming sidekiq
+docker-compose rm -f -v web streaming sidekiq
 docker-compose run --rm web ./bin/rails db:migrate assets:precompile
-docker-compose up -d
+docker-compose up -d web streaming sidekiq
 docker-compose scale sidekiq=3
+docker-compose kill -s HUP nginx
